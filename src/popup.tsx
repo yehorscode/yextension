b399d44b685e3d631fc8e3b0dfc1c8ca91c97cf3
Yehor <omelianenkoegor@gmail.com>
2025-10-29 16:08:40 +0100

    Initial thing

--- src/popup.tsx
+++ src/popup.tsx
@@ -6,7 +6,8 @@ function IndexPopup() {
   return (
     <div className="plasmo-flex plasmo-flex-col plasmo-items-center plasmo-justify-center plasmo-min-w-fit">
       <h1 className="plasmo-font-bold plasmo-text-2xl">yExtension</h1>
-      <span>An extension for all <i>my</i> needs and some things that i made for fun</span>
+      <span>An extension for all <i>my</i> needs and some things that i made for fune</span>
+      <a href="">see features</a>
     </div>
   )
 }
